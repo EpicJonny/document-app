2efdfca4a4f26f2f0b8b7ee00621ca68b9929bf6
Jonathan Mendonca <jonodmend@hotmail.com>
2019-04-21 08:14:10 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+// ...other imports
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DocumentListComponent } from './document-list/document-list.component';
+import { DocumentComponent } from './document/document.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocumentListComponent,
+    DocumentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
